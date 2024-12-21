0f82b57a10650fc46e8bbf804bcc15e40f56a0ee
moomtp <moomtp@gmail.com>
2024-12-21 16:17:08 +0800

    ac.js 1.0

--- functions/ac.js
+++ functions/ac.js
@@ -28,40 +28,42 @@ class AC {
       deviceInfo: this.deviceInfo,
       willReportState: true,
       attributes: {
-        pausable: true,
-        availableModes: [{
-          name: '',
-          name_values: [{
-            name_synonym: ['load'],
-            lang: 'en',
-          }],
-          settings: [{
-            setting_name: 'cool',
-            setting_values: [{
-              setting_synonym: ['cool'],
-              lang: 'en',
-            }]
-          }, {
-            setting_name: 'heat',
-            setting_values: [{
-              setting_synonym: ['heat'],
-              lang: 'en',
-            }]
-          }, {
-            setting_name: 'fan',
-            setting_values: [{
-              setting_synonym: ['fan'],
-              lang: 'en',
-            }]
-          }],
-          ordered: true,
-        }],
-          temperatureRange: {
-          minThresholdCelsius: 16,
-          maxThresholdCelsius: 32
+        availableFanSpeeds: {
+          speeds: [
+            {
+              speed_name: "low_key",
+              speed_values: [
+                {
+                  speed_synonym: [
+                    "Low",
+                    "Slow"
+                  ],
+                  lang: "en"
+                }
+              ]
+            },
+            {
+              speed_name: "high_key",
+              speed_values: [
+                {
+                  speed_synonym: [
+                    "High",
+                    "Speed 2"
+                  ],
+                  lang: "en"
+                }
+              ]
+            }
+          ],
+          ordered: true
         },
-        temperatureUnitForUX: 'C',
-        commandOnlyTemperatureControl: false
+        availableThermostatModes: [
+          "off",
+          "cool",
+          "fan-only",
+          "on"
+        ],
+        thermostatTemperatureUnit: "C"
       },
     };
   }
@@ -69,17 +71,12 @@ class AC {
   async getDeviceStates(snapshotVal) {
     return {
       on: snapshotVal.OnOff.on,
-      currentRunCycle: [{
-        currentCycle: 'rinse',
-        nextCycle: 'spin',
-        lang: 'en',
-      }],
-      currentTotalRemainingTime: 1212,
-      currentCycleRemainingTime: 301,
-      currentModeSettings: {
-        load: snapshotVal.Modes.load
-      },
-      temperatureSetpointCelsius: snapshotVal.TemperatureControl.temperatureSetpointCelsius,
+      currentFanSpeedSetting: snapshotVal.SetFanSpeed.currentFanSpeed,
+      thermostatMode: snapshotVal.thermostatMode,
+      thermostatTemperatureAmbient: 25,
+      thermostatTemperatureSetpoint: snapshotVal.TemperatureControl.temperatureSetpointCelsius,
+
+      // temperatureSetpointCelsius: snapshotVal.TemperatureControl.temperatureSetpointCelsius,
       // temperatureAmbientCelsius: snapshotVal.TemperatureControl.temperatureAmbientCelsius
     };
   }
@@ -90,17 +87,21 @@ class AC {
       case 'action.devices.commands.OnOff':
         state = { path: 'OnOff', state: { on: params.on } };
         break;
-      case 'action.devices.commands.SetModes':
-        state = { path: 'Modes', state: { load: params.updateModeSettings.load } };
+      case 'action.devices.commands.SetFanSpeed':
+        state = { path: 'SetFanSpeed', state: { fanSpeed: params.currentFanSpeed } };
         break;
 
       case 'action.devices.commands.ThermostatTemperatureSetpoint':
         state = {
           path: 'TemperatureControl',
           state: { 
-            temperatureSetpointCelsius: params.temperature
+            thermostatTemperatureSetpoint : params.temperatureSetpointCelsius
           }
         };
+        break;
+
+      case 'action.devices.commands.ThermostatSetMode':
+        state = { path: 'ThermostatMode', state: { thermostatMode: params.currentMode } };
         break;
       default:
         throw new Error(`Command ${command} not supported`);
@@ -111,10 +112,9 @@ class AC {
   getReportStatePayload(snapshot) {
     return {
       on: snapshot.OnOff.on,
-      currentModeSettings: {
-        load: snapshot.Modes.load,
-      },
-      temperatureSetpointCelsius: snapshot.TemperatureControl.temperatureSetpointCelsius,      
+      fanSpeed: snapshot.SetFanSpeed.currentFanSpeed,
+      thermostatMode: snapshot.thermostatMode.currentMode,
+      thermostatTemperatureSetpoint: snapshot.TemperatureControl.temperatureSetpointCelsius,      
     };
   }
 }
